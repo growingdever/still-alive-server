bf3f876c106af7b108bb164b7e9b11458c6c4ebe
loki <growingdever@gmail.com>
2014-11-13 00:07:57 +0900

    implement registing user api

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+var bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/regist', function(req, res) {
+
+router.get('/', function(req, res) {
   var json = {
-    result: 0,
-    message: "success!"
+    result: 1,
+    message: "here is root of auth.js"
   };
   res.send(json);
+});
+
+router.get('/regist', function(req, res) {
+  bcrypt.hash(req.param('password'), 
+    8, 
+    function(err, hash) {
+      var data = { 
+        'userID': req.param('userid'),
+        'password': hash
+      };
+
+      db.User
+        .create(data)
+        .complete(function(err, user){
+          if( err ) {
+            res.send({
+              'success': 0,
+              'message': err
+            });
+          } else {
+            res.send({
+              'success': 1,
+              'user': user
+            })
+          }
+        });
+  });
 });
 
 router.get('/signin', function(req, res) {
   var json = {
     result: 0,
-    message: "success!"
+    message: "/signin"
   };
   res.send(json);
 });
+
 
 module.exports = router;
