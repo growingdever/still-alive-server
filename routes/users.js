83ac7ccee1289b1c13bc7f49e77386523f5a53de
loki <growingdever@gmail.com>
2014-11-13 02:35:22 +0900

    searching user api

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -10,12 +11,28 @@ router.get('/', function(req, res) {
   res.send(json);
 });
 
-router.get('/hello', function(req, res) {
-  var json = {
-    result: 1,
-    message: "hello world!"
-  };
-  res.send(json);
+router.get('/search', function(req, res) {
+  var subs = req.param('keyword');
+  query = "userID LIKE " + '\'' + subs + '%\''; // userID LIKE 'foo%'
+  db.User
+    .findAll({
+      where: [ query ]
+    })
+    .success(function(users) {
+      var arr = [];
+      for (var i = users.length - 1; i >= 0; i--) {
+        var json = {};
+        json.id = users[i].id;
+        json.userID = users[i].userID;
+        json.nickname = users[i].nickname;
+        arr.push( json );
+      };
+
+      res.send({
+        result: RESULT_CODE_SUCCESS,
+        data: arr
+      });
+    });
 });
 
 
