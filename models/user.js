9378140054425e34ca605601e9c7390401f58db5
loki <growingdever@gmail.com>
2014-11-13 20:06:47 +0900

    remove lastUpdateAt field on User

--- models/user.js
+++ models/user.js
@@ -4,7 +4,6 @@ module.exports = function(sequelize, DataTypes) {
     password: DataTypes.STRING,
     nickname: DataTypes.STRING,
     phoneNumber: DataTypes.STRING,
-    lastUpdateAt: DataTypes.DATE,
   }, {
     classMethods: {
       associate: function(models) {
