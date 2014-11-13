43a5d83c4016fe6a61e8e1e45536337f39bcdb29
loki <growingdever@gmail.com>
2014-11-13 16:42:59 +0900

    add enabled flag to request

--- models/request.js
+++ models/request.js
@@ -2,6 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var Request = sequelize.define('Request', {
     userID: DataTypes.STRING,
     targetUserID: DataTypes.STRING,
+    enabled: { type: DataTypes.BOOLEAN, defaultValue: true }
   }, {
     classMethods: {
       associate: function(models) {
